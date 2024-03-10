749e73f5907f033d6f10f33c7b31d0def1afff92
Hardik Suryakant Nikam <hardiknikam7@gmail.com>
2024-03-10 17:05:45 +0530

    new dom objects practice added

---
+++ 14_DOM_getelementbyid/script.js
@@ -0,0 +1,14 @@
+// continuation of previous dom lecture
+
+// getElementById
+const ele = document.getElementById("username")
+console.log(ele.innerText)
+ele.innerText = "John Doe";
+
+//add classes with the help of javascript
+
+
+ele.classList.add('red-color','underline')
+
+
+
