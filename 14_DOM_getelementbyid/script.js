53bbc644a00d34f8f3e94ed1098fc744639f9d81
Hardik Suryakant Nikam <hardiknikam7@gmail.com>
2024-03-11 18:13:16 +0530

    dom_getElementById

--- 14_DOM_getelementbyid/script.js
+++ 14_DOM_getelementbyid/script.js
@@ -1,14 +1,49 @@
 // continuation of previous dom lecture
 
+
 // getElementById
 const ele = document.getElementById("username")
-console.log(ele.innerText)
+console.log(ele);
+// console.log(ele.innerText)
 ele.innerText = "John Doe";
 
 //add classes with the help of javascript
 
 
-ele.classList.add('red-color','underline')
+ele.classList.add('red-color','italic') 
+
+console.log(ele.classList)  // returns  a DOMTokenList object containing class names in the
+ 
 
 
+//Inline Style Using Javascript
 
+ele.style.textDecoration = "underline";
+ele.style.color = "blue"
+console.log(ele.parentElement)
+ele.parentElement.style.background = "purple"
+
+//set Attribute
+
+ele.setAttribute("aria","123")
+
+
+//Remove element from DOM
+ele.remove();
+// Rmoving a class from a Classlist
+
+// function myFunction(a){
+//     let aa = a.length;
+//     aa=aa/2;
+//     let aaa ;
+//     for ( let i = 0 ; i <aa ; i++){
+//        aaa.push(a[i]);
+//     }
+    
+//     return aaa;
+// }
+
+// let ii = "1233";
+// let il = myFunction(ii);
+// // console.log(ii.length)
+// console.log(il);
